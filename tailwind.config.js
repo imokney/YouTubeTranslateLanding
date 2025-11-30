814cef5c6b9f00aa89d84da553d37b8334ebbde8
ih8myanx <spang.252@gmail.com>
2025-11-30 21:29:59 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// tailwind.config.cjs или .js
+// tailwind.config.js
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -7,11 +7,14 @@ module.exports = {
       keyframes: {
         marquee: {
           "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-50%)" }, // едем ровно на половину ширины трека
+          "100%": { transform: "translateX(-50%)" },
         },
       },
       animation: {
-        marquee: "marquee 25s linear infinite", // можешь ускорить/замедлить
+        marquee: "marquee 25s linear infinite",
+      },
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
       },
     },
   },
